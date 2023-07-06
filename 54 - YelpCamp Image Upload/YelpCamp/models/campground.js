c3d3bcccbd81321df60fd1cde49d184e4d836f7d
Lucas Mella <lucasmella16@gmail.com>
2023-07-06 16:26:14 -0300

    Corrigindo que sepa commitei errado

--- 54 - YelpCamp Image Upload/YelpCamp/models/campground.js
+++ 54 - YelpCamp Image Upload/YelpCamp/models/campground.js
@@ -3,15 +3,18 @@ const Review = require("./review");
 const Schema = mongoose.Schema;
 
 
+const ImageSchema = new Schema({
+        url: String,
+        filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace("/upload", "/upload/w_200");
+});
 
 const CampGroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -37,5 +40,6 @@ CampGroundSchema.post("findOneAndDelete", async function (doc) {
         });
     }
 });
+
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
