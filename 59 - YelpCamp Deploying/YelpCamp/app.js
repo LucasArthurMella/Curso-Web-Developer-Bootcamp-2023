4329c31a8776d5711a5f379e93ebb74980562427
Lucas Mella <lucasmella16@gmail.com>
2023-07-10 00:08:17 -0300

    Mudado algumas coisas pra configuracao do deploy

--- 59 - YelpCamp Deploying/YelpCamp/app.js
+++ 59 - YelpCamp Deploying/YelpCamp/app.js
@@ -22,7 +22,8 @@ const reviewRoutes = require("./routes/reviews");
 const MongoStore = require('connect-mongo');
 
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+
 const userRoutes = require("./routes/users");
 
 mongoose.set('strictQuery', true)
@@ -52,11 +53,13 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret
     }
 });
 
@@ -67,7 +70,7 @@ store.on("error", function(e){
 const sessionConfig = {
     store,
     name: "session",
-    secret: "thisshouldbeabettersecret!",
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
