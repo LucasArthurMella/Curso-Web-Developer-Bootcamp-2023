52819463325372db21563fb58ddd64555a4576a1
Lucas Mella <lucasmella16@gmail.com>
2023-07-15 03:12:35 -0300

    Finalizado algumas partes de component design

---
+++ 68 - Component Design/component-design/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Dice from './Dice'
+
+function App() {
+
+  return (
+    <>
+      {/* <Lucky7/> */}
+      <Dice dice={[2,3,4]}/>
+    </>
+  )
+}
+
+export default App
